e526f7e1a48e49318fd269d8bd6f3bf0310d9ba0
Achyuta <achyutathegreat@gmail.com>
2022-10-30 10:29:06 +0530

    Added ingredient select

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -22,6 +22,29 @@ const Sidebar = () => {
 				setMaxCook('')
 		}
 
+		const [ingredients, setIngredients] = useState<string[]>([
+				'Ragi',
+				'Wheat Flour',
+				'Rava',
+				'Apple',
+				'Orange'
+		])
+
+		const [selected, setSelected] = useState<string[]>([])
+		const [ingredientSelected, setIngredientSelected] = useState<string>('')
+
+		const addIngredientFilter = (ingredient: string) => {
+				if (!ingredient) return
+				setIngredients(ingredients.filter(el => el != ingredient))
+				setSelected(selected.concat([ingredient]))
+				setIngredientSelected('')
+		}
+
+		const removeIngredientFilter = (ingredient: string) => {
+				setSelected(selected.filter(el => el != ingredient))
+				setIngredients(ingredients.concat([ingredient]))
+		}
+
 		return (
 				<div className='h-[100vh] border border-l-gray-300 w-full p-2'>	
 						<div className='text-2xl font-bold text-center m-2'> Recipe SA </div>
@@ -36,6 +59,17 @@ const Sidebar = () => {
 										<option value=""></option>
 										{courses.map(el => <option value={el}>{el}</option>)}
 								</select>
+								<label className="inline-block text-lg font-semibold border-b-2 border-b-yellow-400 m-1">Ingredient Filter</label>
+								<select value={ingredientSelected} className='bg-gray-200 border-none block rounded-full w-full my-1' defaultValue={''} onChange={e => addIngredientFilter(e.target.value)}>
+										<option value=""></option>
+										{ingredients.map(el => <option value={el}>{el}</option>)}
+								</select>
+								{selected.map(el => 
+										<div key={el} className="m-1 p-2 text-md font-light border rounded-sm flex justify-between select-none">
+										<div>{el}</div>
+												<div onClick={() => removeIngredientFilter(el)} className='px-2 cursor-pointer text-lg'>&times;</div>
+										</div>
+								)}
 								<label className="inline-block text-lg font-semibold border-b-2 border-b-yellow-400 m-1">Cook Time</label>
 								<div>
 										<input value={minCook} placeholder='(in min)' className='inline w-24 rounded-full m-1 bg-gray-200 border-none' onChange={e => setMinCook(Number(e.target.value))} type="number" min="10" max="500"/>
@@ -48,7 +82,8 @@ const Sidebar = () => {
 										<div className='inline mx-1 text-xl'>-</div>
 										<input value={maxPrep} placeholder='(in min)' className='inline w-24 rounded-full m-1 bg-gray-200 border-none' onChange={e => setMaxPrep(Number(e.target.value))} type="number" min="10" max="500"/>
 								</div>
-								<input className='w-full rounded-lg mx-auto my-3 text-lg bg-yellow-400 font-semibold p-2' type="submit" onSubmit={() => {}}/>
+								<input className='w-full rounded-lg mx-auto mt-3 text-lg bg-yellow-400 font-semibold p-2' type="submit" onSubmit={() => {}}/>
+								<input className='w-full rounded-lg mx-auto my-1 text-lg bg-yellow-400 font-semibold p-2' type="reset" onClick={clearFilter}/>
 						</form>
 
 				</div>
---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, useReducer, useEffect } from 'react'
+
+
+type AuthContextType = {
+		dispatch: React.Dispatch<{
+				type: string;
+				payload?: string;
+		}>
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+export const authReducer = (state: any, action: {type: string, payload: string}) => {
+		switch (action.type) {
+				case 'LOGIN':
+						return { user: action.payload }
+				case 'LOGOUT':
+						return { user: null }
+				default:
+				return state
+		}
+}
+
+interface Props {
+		children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined
+}
+
+export const AuthContextProvider = ({ children }: Props) => {
+		const [state, dispatch] = useReducer(authReducer, { 
+				user: null
+		})
+
+		useEffect(() => {
+				const user = JSON.parse(localStorage.getItem('user')!)
+
+				if (user) {
+						dispatch({ type: 'LOGIN', payload: user }) 
+				}
+		}, [])
+
+		console.log('AuthContext state:', state)
+
+		return (
+				<AuthContext.Provider value={{ ...state, dispatch }}>
+						{ children }
+				</AuthContext.Provider>
+		)
+
+}
