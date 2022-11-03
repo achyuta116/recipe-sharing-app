badcd7eab41f54fdf306b3c0042fac853bf578a3
Achyuta <achyutathegreat@gmail.com>
2022-11-03 22:36:30 +0530

    Frontend setup for loading recipes

---
+++ frontend/src/hooks/useRecipesContext.ts
@@ -0,0 +1,12 @@
+import { RecipesContext } from "../contexts/RecipesContext"
+import { useContext } from "react"
+
+export const useRecipesContext = () => {
+		const context = useContext(RecipesContext)
+
+		if(!context) {
+				throw Error('useRecipesContext must be used inside an RecipesContextProvider')
+		}
+
+		return context
+}
