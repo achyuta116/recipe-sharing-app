64f654d56a871888a01fff4a7e4d596dd99c6e23
Achyuta <achyutathegreat@gmail.com>
2022-11-10 23:18:08 +0530

    Login Page CSS done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import Card from './components/Card'
 import Sidebar from './components/Sidebar'
 import SideNav from './components/SideNav'
 import Create from './pages/Create'
+import Login from './pages/Login'
 
 const App = () => {
     return (
         <div className='grid grid-cols-10'>
-            <Create/>
+            <Login/>
         </div>
     )
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,31 +6,43 @@ const Login = () => {
     const [signupName, setsignupName] = useState('')
     const [signupUsername, setsignupUsername] = useState('')
     const [signupPwd, setsignupPwd] = useState('')
+    const [showSignup, setShowSignup] = useState(true)
     return (
-        <div className='grid place-items-center h-[100vh] w-full'>	
-            <div className='grid grid-cols-2 rounded-lg border-gray-500 border-2 w-[50vw] h-[75vh] overflow-clip'>
-                <div className='grid col-span-1 place-items-center border-0 border-r border-r-gray-500'>
-                    <form>
-                        <div className='text-center text-2xl font-bold'>Login</div>
-                        <input type="text" onChange={e => setloginUsername(e.target.value)} className='bg-gray-200 border-none block rounded-full w-full my-1' placeholder='Username'/>
-                        <input type="password" onChange={e => setloginPwd(e.target.value)} className='bg-gray-200 border-none block rounded-full w-full my-1' placeholder='Password'/>
-                        <div className='grid place-items-center'>
-                            <div className='inline-block p-2 bg-yellow-400 font-semibold text-center rounded-full'>Submit</div>
-                        <div className='text-sm text-red-500'></div>
-                        </div>
-                    </form>
+        <div className='grid place-items-center h-[100vh] w-[100vw] bg-gradient-to-b from-black'>	
+            <div className='grid grid-cols-3 rounded-lg w-[69vw] h-[75vh] overflow-clip shadow-xl'>
+                <div className='col-span-2 grid content-center justify-items-end p-5 bg-white'>
+                    <div className="text-6xl font-extrabold text-right">Recipes </div> 
+                        <div className="text-6xl font-extrabold text-right"> App </div>
+                    <div className='text-right mt-3 w-96'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus libero exercitationem architecto facere perferendis earum iste quod voluptatibus nihil? Voluptatem corrupti quae id obcaecati aut minima blanditiis laborum voluptate laudantium.</div>
                 </div>
-                <div className='grid col-span-1 place-items-center bg-yellow-400 border-0 border-l border-l-gray-500'>
-                    <form>
-                        <div className='text-center text-2xl font-bold'>Signup</div>
-                        <input type="text" onChange={e => setsignupName(e.target.value)} className='block w-full my-1 border-gray-200 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Name'/>
-                        <input type="text" onChange={e => setsignupUsername(e.target.value)}className='block rounded-full w-full my-1 border-b' placeholder='Username'/>
-                        <input type="password" onChange={e => setsignupPwd(e.target.value)} className='block rounded-full w-full my-1 border-b' placeholder='Password'/>
-                        <div className='grid place-items-center'>
-                            <div className='inline-block p-2 bg-yellow-400 font-semibold text-center rounded-full'>Submit</div>
-                        <div className='text-sm text-red-500'></div>
+                <div className='col-span-1 bg-yellow-400'>
+                        <div className="flex mt-1 space-x-8 justify-center items-center">
+                            <div className={'text-xl pt-2 border-b-2 cursor-pointer border-black' + (!showSignup? ' border-transparent' : '')} onClick={() => setShowSignup(true)}>Sign Up</div>
+                            <div className={'text-xl pt-2 border-b-2 cursor-pointer border-black ' + (showSignup? ' border-transparent' : '')} onClick={() => setShowSignup(false)}>Login</div>
                         </div>
-                    </form>
+                {!showSignup && 
+                    <div className='grid place-items-center h-full'>
+                        <form>
+                            <input type="text" onChange={e => setloginUsername(e.target.value)} className='block placeholder-gray-700 hover:placeholder-black w-full bg-transparent my-1 border-gray-700 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Username'/>
+                            <input type="password" onChange={e => setloginPwd(e.target.value)} className='block placeholder-gray-700 hover:placeholder-black w-full bg-transparent my-1 border-gray-700 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Password'/>
+                            <div className='grid place-items-center'>
+                                <div className='mt-3 cursor-pointer inline-block py-2 px-4 bg-black text-white font-semibold text-center rounded-full'>Submit</div>
+                            </div>
+                        </form>
+                    </div>
+            }
+                {showSignup && 
+                    <div className='grid place-items-center h-full'>
+                        <form>
+                            <input type="text" onChange={e => setsignupName(e.target.value)} className='block placeholder-gray-700 hover:placeholder-black w-full bg-transparent my-1 border-gray-700 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Name'/>
+                            <input type="text" onChange={e => setsignupUsername(e.target.value)}className='block placeholder-gray-700 hover:placeholder-black w-full bg-transparent my-1 border-gray-700 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Username'/>
+                            <input type="password" onChange={e => setsignupPwd(e.target.value)} className='block placeholder-gray-700 hover:placeholder-black w-full bg-transparent my-1 border-gray-700 border-0 border-b-2 focus:ring-0 focus:border-black' placeholder='Password'/>
+                            <div className='grid place-items-center'>
+                                <div className='mt-3 cursor-pointer inline-block py-2 px-4 bg-black text-white font-semibold text-center rounded-full'>Submit</div>
+                            </div>
+                        </form>
+                    </div>
+            }
                 </div>
             </div>
         </div>
