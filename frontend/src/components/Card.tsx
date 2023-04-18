b989eb9267b8d01932b2a53d7fe5f8df0e938b7b
Achyuta <achyutathegreat@gmail.com>
2023-04-18 22:33:14 +0530

    back to blue

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -21,14 +21,14 @@ const Card = ({ recipeName, imageUrl, author, prepTime, cookTime, course, cuisin
                     <img className='w-full hover:scale-105 transition-all ease-out' src={imageUrl} alt='recipe-img'/>
                 </div>
                 <div className='absolute top-0 flex space-x-2 p-2 w-full'>
-                    <div className='badge bg-yellow-400 cursor-default'>{cuisine}</div>
-                    <div className='badge bg-yellow-400 cursor-default'>{course}</div>
+                    <div className='badge bg-blue-400 cursor-default'>{cuisine}</div>
+                    <div className='badge bg-blue-400 cursor-default'>{course}</div>
                     <div className='flex items-end flex-1 flex-col space-y-1 text-gray-700'>
                         <div className='badge bg-gray-200 cursor-default'>{prepTime}min 🔪</div>
                         <div className='badge bg-gray-200 cursor-default'>{cookTime}min 🕙</div>
                     </div>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute right-4 top-40 w-14 h-14 bg-yellow-400 rounded-full p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute right-4 top-40 w-14 h-14 bg-blue-400 rounded-full p-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
                 </svg>
                 <div className='p-2 border-gray-50 text-gray-700'>
