d1e95f4e5f90fa41c16dd20c1002a01072a19130
bgsourav <73071764+bgsourav@users.noreply.github.com>
2023-04-22 20:52:01 +0530

    LoginForm Final Changes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -34,12 +34,12 @@ const Login = () => {
     return (
         <div className="grid place-items-center h-[100vh] w-[100vw] bg-[url('https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2788&q=80')] bg-cover bg-no-repeat">	
             <div className='grid grid-cols-3 rounded-xl w-[69vw] overflow-clip shadow-2xl'>
-                <div className='col-span-2 grid content-center justify-items-end p-5 backdrop-blur-md bg-white bg-opacity-20'>
-                    <div className="text-6xl font-extrabold text-right">Recipes</div> 
-                    <div className="text-6xl font-extrabold text-right">App</div>
+                <div className='col-span-2 grid content-center justify-items-end p-5 backdrop-blur-md bg-gray-900 bg-opacity-20'>
+                    <div className="text-5xl font-extrabold text-right">Recipes</div> 
+                    <div className="text-5xl font-extrabold text-right">App</div>
                     <div className='text-right mt-3 w-96'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus libero exercitationem architecto facere perferendis earum iste quod voluptatibus nihil? Voluptatem corrupti quae id obcaecati aut minima blanditiis laborum voluptate laudantium.</div>
                 </div>
-                <div className='flex flex-col col-span-1 bg-blue-400 h-[75vh]'>
+                <div className='flex flex-col col-span-1 bg-blue-300 h-[75vh]'>
                         <div className="grid grid-cols-2 border-b-2 border-b-white bg-blue-500">
                             <div className={'text-xl py-2 w-full text-center cursor-pointer' + (showSignup? ' bg-blue-400' : '')} onClick={() => setShowSignup(true)}>Sign Up</div>
                             <div className={'text-xl py-2 w-full text-center cursor-pointer' + (!showSignup? ' bg-blue-400' : '')} onClick={() => setShowSignup(false)}>Login</div>
